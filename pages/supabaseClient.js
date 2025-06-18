b466fba8e91dbc78dae9bacc2afdee67ae3288a8
Maincooninfinity <ceo@maincoonfashion.com>
2025-06-17 17:10:16 -0700

    Update supabaseClient.js

--- pages/supabaseClient.js
+++ pages/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+const supabaseUrl = 'https://tuowkzxyvigznctkihdm.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1b3drenh5dmlnem5jdGtpaGRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxOTgwODEsImV4cCI6MjA2NTc3NDA4MX0.7_coQ_1sP0vycNmLjt9bq8NB8YX0LZUFq-VJUZHFudc'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
