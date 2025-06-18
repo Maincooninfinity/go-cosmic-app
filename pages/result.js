e2f9ba4025f84d08e7ca3f08b365f3cd1e04a0ac
Maincooninfinity <ceo@maincoonfashion.com>
2025-06-17 19:12:39 -0700

    Update result.js

--- pages/result.js
+++ pages/result.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import supabase from '../supabaseClient'
+import supabase from '../supabaseClient
 export default function Home() {
   useEffect(() => {
     const testInsert = async () => {
